a1cd22c653caf869dd73d36dca4ae5926ddffafa
Ruihao LI <>
2024-01-30 01:08:16 -0500

    create project 1 document, working on my project.

---
+++ project 1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Resource Allocation for Diverse Emergency Services in different Times and Townships
+
+# The research topic’s variable Y is the type of emergency and variable X is township, time, and address. By examining these relationships, the study aims to enhance society’s well-being and ensure citizens’ safety. Make the right kind of help available where and when it's most needed.
+# Choosing this topic stems from realizing the critical role that efficient emergency 
+# services play in our daily lives. Inefficiency and misallocations in emergency resources can have severe consequences, including loss of life, prolonged suffering, and increased societal costs. Additionally, the findings from this study could inform adjustments in local infrastructure as well.
+
+# In[ ]:
+
+
+
+
